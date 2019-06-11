b966ffa97fe721c0dc37a64c00222fcb3f19f0ae
laurabo <bodelllaura@gmail.com>
2019-06-11 16:32:11 -0400

    added line of code to each action in room controller for finding room by id

--- app/controllers/room_controller.rb
+++ app/controllers/room_controller.rb
@@ -1,5 +1,6 @@
 class RoomController < ApplicationController
   def index
+    @rooms = Rooms.all 
   end
 
   def new
@@ -9,23 +10,30 @@ class RoomController < ApplicationController
   end
 
   def listing
+    @room = Room.find(params[:id])
   end
 
   def pricing
+    @room = Room.find(params[:id])
   end
 
   def description
+    @room = Room.find(params[:id])
   end
 
   def photo_upload
+    @room = Room.find(params[:id])
   end
 
   def amenities
+    @room = Room.find(params[:id])
   end
 
   def location
+    @room = Room.find(params[:id])
   end
 
   def update
+    @room = Room.find(params[:id])
   end
 end
