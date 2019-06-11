f4fed66dd25c3ded35bfb5ffc6c5d084e5156076
laurabo <bodelllaura@gmail.com>
2019-06-11 16:05:34 -0400

    generated new room controller. routes created in config folder. new view folder created- to be removed later

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'room/index'
+  get 'room/new'
+  get 'room/create'
+  get 'room/listing'
+  get 'room/pricing'
+  get 'room/description'
+  get 'room/photo_upload'
+  get 'room/amenities'
+  get 'room/location'
+  get 'room/update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resources :pages
 
