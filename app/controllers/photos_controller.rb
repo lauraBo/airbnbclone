b1868a7fbc6b9e53b2f7540e12ee5646ea6faf33
laurabo <bodelllaura@gmail.com>
2019-06-12 16:34:00 -0400

    added destroy action to photo controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -14,4 +14,14 @@ class PhotosController < ApplicationController
         end
 
     end 
+
+    def destroy
+        @photo = Photo.find(params[:id])
+        room = @photo.room 
+
+        @photo.destroy
+        @photos = Photo.where(room_id: room.id)
+
+        respond_to :js
+    end 
 end 
