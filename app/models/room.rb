72d4ff7dc10b4b41db3e693a351cf715cb342916
laurabo <bodelllaura@gmail.com>
2019-06-14 18:37:03 -0400

    added guest and host review models as child of Review class

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,11 +1,17 @@
 class Room < ApplicationRecord
     has_many :photos 
     has_many :reservations
+    has_many :guest_reviews
 
     validates :home_type, presence: true
     validates :room_type, presence: true
     validates :accommodate_type, presence: true
     validates :bed_room, presence: true
     validates :bath_room, presence: true
+
+
+    def average_rating
+        guest_reviews.count == 0 ? 0 : guest_reviews.average(:star).round(2).to_i
+    end  
 
 end
