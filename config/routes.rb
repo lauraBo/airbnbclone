b1fc30bfe01c708d24554a6a493303ef416e9120
laurabo <bodelllaura@gmail.com>
2019-06-16 13:30:00 -0400

    deleted pages controller, pages route in config file and pages view page. generated new controller and action from terminal

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   
+  get 'pages/home'
   devise_for :users
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root 'pages#home'
+
 resources :rooms, except: [:edit] do
   member do
     get 'listing'
