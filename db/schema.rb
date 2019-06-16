5ec95b935df3422b730b88f26fb37979d97796be
laurabo <bodelllaura@gmail.com>
2019-06-16 16:02:21 -0400

    added fullname column to User model. added devise validations to application controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_14_225943) do
+ActiveRecord::Schema.define(version: 2019_06_16_195157) do
 
   create_table "photos", force: :cascade do |t|
     t.integer "room_id"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 2019_06_14_225943) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "fullname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
