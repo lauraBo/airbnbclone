97f94fd3e42cf86731a9468673321bbdaf32b95a
laurabo <bodelllaura@gmail.com>
2019-06-20 19:46:48 -0400

    updated index action and private method in room controller. added gmail email confirmation when new user registers

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,20 +1,20 @@
 class RoomController < ApplicationController
-  before_action #authenticate user?
+  before_action :authenticate_user!, except: [:show]
   before_action :set_room, except: [:index, :new, :create]
 
-  def index #public page
-    @rooms = Rooms.all 
+  def index 
+    @rooms = current_user.rooms
   end
 
   def show #public page
   end 
 
-  def new #private page
-    @room = Room.new(room_params)
+  def new 
+    @room = current_user.rooms.build
   end
 
   def create #private page
-    @room = Room.new(room_params)
+    @room = current_user.rooms.build(room_params)
     if @room.save
       redirect_to listing_room_path(@room), notice: "listing saved"
     else
@@ -63,6 +63,10 @@ class RoomController < ApplicationController
     def set_room
       @room = Room.find(params[:id])
     end 
+
+    def is_authorised
+      redirect_to root_path, alert: "You don't have permission" unless current_user.id == @room.user_id
+    end
 
     def room_params
       params.require(:room).permit(:home_type, :room_type, :accommodate, :bed_room, :bath_room, :listing_name, :summary, :address, is_tv, :is_kitchen, :is_air, 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_16_195157) do
+ActiveRecord::Schema.define(version: 2019_06_20_223809) do
 
   create_table "photos", force: :cascade do |t|
     t.integer "room_id"
@@ -79,6 +79,10 @@ ActiveRecord::Schema.define(version: 2019_06_16_195157) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "fullname"
+    t.string "confirmation_token"
+    t.datetime "confirmation_at"
+    t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
