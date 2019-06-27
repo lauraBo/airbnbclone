7051693bbdd1246706c17cc661d0bc353f7ac24d
laurabo <bodelllaura@gmail.com>
2019-06-27 14:45:50 -0400

    updated rooms controller with instant option inside room params. updated room form partial with two new flash notices depending on instant or request booking

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,37 +1,47 @@
 class ReservationsController < ApplicationController
     before_action :authenticate_user!
-  
- 
+
+
 
     def create
       room = Room.find(params[:room_id])
-  
+
       if current_user == room.user
         flash[:alert] = "You cannot book your own property!"
       else
         start_date = Date.parse(reservation_params[:start_date])
         end_date = Date.parse(reservation_params[:end_date])
         days = (end_date - start_date).to_i + 1
-  
+
         @reservation = current_user.reservations.build(reservation_params)
         @reservation.room = room
         @reservation.price = room.price
         @reservation.total = room.price * days
-        @reservation.save
-  
-        flash[:notice] = "Booked Successfully!"
+      #  @reservation.save
+
+      if @reservation.save
+        if room.Request?
+          flash[:notice] = "Request sent successfully!"
+        else
+          @reservation.Approved!
+          flash[:notice] = "Reservation created successfully!"
+        end
+      else
+        flash[:notice] = "Cannot make a reservation at this time!"
+      end
+
       end
       redirect_to room
     end
-  
+
     def your_trips
       @trips = current_user.reservations.order(start_date: :asc)
-    end 
+    end
 
     def your_reservations
       @rooms = current_user.rooms
-    end 
-  
+    end
+
     private
       def reservation_params
         params.require(:reservation).permit(:start_date, :end_date)
